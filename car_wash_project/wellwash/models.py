6bdca15d043512c35b086e368c96711980454ead
Giorgi1971 <giooorgi7@gmail.com>
2021-01-26 02:47:28 +0400

    migrate Django tamplate files, then going migrate your apps files

--- car_wash_project/wellwash/models.py
+++ car_wash_project/wellwash/models.py
@@ -1,3 +1,20 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    city = models.CharField(max_length=255)
+    street_address = models.CharField(max_length=255)
+    zip = models.CharField(max_length=7)
+
+    def __str__(self):
+        return f'{self.city} : {self.street_address}: {self.zip}'
+
+
+class WashObject(models.Model):
+    title = models.CharField(max_length=255, verbose_name=_('Title'))
+    location = models.OneToOneField('wellwash.Location', on_delete=models.PROTECT, related_name='store')
+    books = models.ManyToManyField(to='store.Book', blank=True, related_name='stores', through='store.StoreToBook')
+
+    def __str__(self):
+        return self.title

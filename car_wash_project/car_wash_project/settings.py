687c236b164e021a01eb69ad4ca872c83d3a3f9d
Giorgi1971 <giooorgi7@gmail.com>
2021-02-04 00:13:05 +0400

    Many things are copyed, db was deleted. copy mainly from tcotne user

--- car_wash_project/car_wash_project/settings.py
+++ car_wash_project/car_wash_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # apps added by me:
+    'user.apps.UserConfig',
     'wellwash.apps.WellwashConfig'
 ]
 
@@ -120,3 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'static_common'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
+
+AUTH_USER_MODEL = 'user.User'
